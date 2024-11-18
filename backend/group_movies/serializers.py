ec24c8b5765dfe41d08ee33fe2f2fb32274b13a4
김은영 <kimeunyoung-0425@naver.com>
2024-11-19 05:22:29 +0900

    Group View 로직 작성 중

--- backend/group_movies/serializers.py
+++ backend/group_movies/serializers.py
@@ -1,2 +1,37 @@
 from rest_framework import serializers
 from .models import Movie, Group, GroupMovie, Article, Comment
+from django.contrib.auth import get_user_model
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+        read_only_fields = ('include_members',)
+
+
+class GroupMovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupMovie
+        fields = '__all__'
+        read_only_fields = ('group', 'movie')
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = ('user', 'like_users', 'group_movie')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('user', 'article')
